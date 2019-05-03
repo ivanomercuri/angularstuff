f64750fc852a41e85cd44846db9a1898da0cfe5e
ivanomercuri <ivano.mercuri@gmail.com>
2019-05-03 18:15:30 +0200

    UMS

--- ums/src/app/users/users.component.ts
+++ ums/src/app/users/users.component.ts
@@ -2,6 +2,38 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-users',
-  template: '<h2>Users</h2>'
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
 })
-export class UsersComponent { }
+export class UsersComponent {
+  title = 'Users';
+  users = [
+    {
+      name: 'Ivano',
+      lastname: 'Mercuri',
+      email: 'a@t.et',
+      fiscalcode: 'abc',
+      province: 'ASD' ,
+      phone: '092',
+      age: 1
+    } ,
+    {
+      name: '1Ivano',
+      lastname: '1Mercuri',
+      email: '1a@t.et',
+      fiscalcode: '1abc',
+      province: '1ASD' ,
+      phone: '1092',
+      age: 2
+    } ,
+    {
+      name: '2Ivano',
+      lastname: '2Mercuri',
+      email: '2a@t.et',
+      fiscalcode: '2abc',
+      province: '2ASD' ,
+      phone: '2092',
+      age: 3
+    }
+  ];
+}
